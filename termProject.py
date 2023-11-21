f005b665b8b0f7d652f5117864ded9b8d513eec0
MeghaJasti <151463040+MeghaJasti@users.noreply.github.com>
2023-11-21 14:30:01 -0500

    Update termProject.py

--- termProject.py
+++ termProject.py
@@ -1,57 +1,97 @@
 from cmu_graphics import *
 
-class Board:
-    #create a blank board
-    def __init__(self):
-        grid = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
-        fill = False
+#pawn class, checks if piece moved up/down one
+class Pawn:
+    def __init__(self, color, row, col):
+        self.color = color
+        self.row = row
+        self.col = col
+
+    def validMove(self, newRow, newCol):
+        if self.col != newCol:
+            return False
+        if self.color == "white":
+            return self.row - 1 == newRow
+        if self.color == "black":
+            return self.row + 1 == newRow
+        return False
     
-    #add a piece to the grid
-    def addPiece(self, piece, row, col):
-        self.grid[row][col] = piece
-    
-    #check if a player won a board
-    def threeInRow(self, piece):
-        #filled row
-        filled = [piece, piece, piece]
-        #check diagonals
-        leftToRightDiagonal = [self.grid[0][0], self.grid[1][1], self.grid[2][2]]
-        rightToLeftDiagonal = [self.grid[0][2], self.grid[1][1], self.grid[2][0]]
-        if leftToRightDiagonal == filled or rightToLeftDiagonal == filled:
-            self.fill = True
+#rook class, checks if piece moved in a line
+class Rook:
+    def __init__(self, color, row, col):
+        self.color = color
+        self.row = row
+        self.col = col
+
+    def validMove(self, newRow, newCol):
+        if self.row == newRow and self.col != newCol:
             return True
-        for i in [0, 1, 2]:
-            #check rows
-            if self.grid[i] == filled: 
-                self.fill = True
-                return True
-            #check columns
-            col = [self.grid[i][0], self.grid[i][1], self.grid[i][2]]
-            if col == filled:
-                self.fill = True
-                return True
+        if self.row != newRow and self.col == newCol:
+            return True
+        return False
+
+#knight class, checks if piece moved in an L
+class Knight:
+    def __init__(self, color, row, col):
+        self.color = color
+        self.row = row
+        self.col = col
     
-    #check if the board is full
-    def full(self):
-        for row in [0, 1, 2]:
-            for col in [0, 1, 2]:
-                if self.grid[row][col] == "-":
-                    return False
-        self.fill = True
-        return True
-    
-#graphics
+    def validMove(self, newRow, newCol):
+        for drow in [-2, 2]:
+            for dcol in [-1, 1]:
+                if self.row + drow == newRow and self.col + dcol == newCol:
+                    return True
+        for dcol in [-2, 2]:
+            for drow in [-1, 1]:
+                if self.row + drow == newRow and self.col + dcol == newCol:
+                        return True
+        return False
 
-def onAppStart(app):
-    pass
 
-def onKeyPress(app, key):
-    pass
+#bishop class, checks if piece moved diagonally
+class Bishop:
+    def __init__(self, color, row, col):
+        self.color = color
+        self.row = row
+        self.col = col
 
-def redrawAll(app):
-    pass
+    def validMove(self, newRow, newCol):
+        drow = abs(newRow - self.row)
+        dcol = abs(newCol - self.col)
+        if drow == dcol: 
+            return True
+        return False
 
-def main():
-    runApp()
+#king class, checks if piece moved one square
+class King:
+    def __init__(self, color, row, col):
+        self.color = color
+        self.row = row
+        self.col = col
 
-main()
+    def validMove(self, newRow, newCol):
+        for drow in [-1, 0, 1]:
+            for dcol in [-1, 0, 1]:
+                if (drow, dcol) != (0, 0):
+                    if self.row + drow == newRow and self.col + dcol == newCol:
+                        return True
+        return False
+
+#queen class, checks if piece moved in a line or diagonally
+class Queen:
+    def __init__(self, color, row, col):
+        self.color = color
+        self.row = row
+        self.col = col
+
+    def validMove(self, newRow, newCol):
+        if self.row == newRow and self.col != newCol:
+            return True
+        if self.row != newRow and self.col == newCol:
+            return True
+        drow = abs(newRow - self.row)
+        dcol = abs(newCol - self.col)
+        if drow == dcol: 
+            return True
+        return False
